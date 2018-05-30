f6a7b20e536aa3871f25e2e7687556a8d81474b5
MichaelDaum <daum@michaeldaumconsulting.com>
2018-05-30 14:28:50 +0200

    Item14700: initial version of a qr code generator

---
+++ lib/Foswiki/Contrib/JQRCodeContrib/Config.spec
@@ -0,0 +1,10 @@
+# ---+ Extensions
+# ---++ JQueryPlugin
+# ---+++ Extra plugins
+# **STRING**
+$Foswiki::cfg{JQueryPlugin}{Plugins}{QRCode}{Module} = 'Foswiki::Contrib::JQRCodeContrib::Core';
+
+# **BOOLEAN**
+$Foswiki::cfg{JQueryPlugin}{Plugins}{QRCode}{Enabled} = 1;
+
+1;
---
+++ pub/System/JQRCodeContrib/init.uncompressed.js
@@ -0,0 +1,16 @@
+/* init */
+"use strict";
+jQuery(function($) {
+   var defaults = {
+      "size": 100,
+      "ecLevel": "H"
+   };
+   $(".qrcode:not(.qrcode_inited)").livequery(function() {
+      var $this = $(this),
+          text = $this.text(),
+          opts = $.extend({text: text}, defaults, $this.data());
+      $this.empty();
+      $this.addClass("qrcode_inited");
+      $this.qrcode(opts);
+   });
+});
